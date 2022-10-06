4d3bc73c1a60e2aea9647a329bb919df8d8b39cf
cheungty1 <99298338+cheungty1@users.noreply.github.com>
2022-10-06 15:12:34 +1100

    Update weather.js

--- src/commands/Information/weather.js
+++ src/commands/Information/weather.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const { EmbedBuilder } = require("discord.js");
 async function getWeather(query) {
   try {
-  // go weather api call
     const weather = await axios.get(
       `https://goweather.herokuapp.com/weather/${query}`
     );
@@ -14,6 +13,68 @@ async function getWeather(query) {
 }
 const capitalizeFirstLetter = (query) => {
   return query.charAt(0).toUpperCase() + query.slice(1);
+};
+const weekday = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+const d = new Date();
+let symbol;
+const weatherSymbol = (description) => {
+  switch (description) {
+    case "Sunny":
+      symbol = "☀️";
+      return symbol;
+    case "Light rain":
+      symbol = "🌦";
+      return symbol;
+    case "Light rain shower":
+      symbol = "🌦";
+      return symbol;
+    case "Partly cloudy":
+      symbol = "⛅️";
+      return symbol;
+    case "Moderate snow":
+      symbol = "❄️";
+      return symbol;
+    case "Clear":
+      symbol = "🌞";
+      return symbol;
+    default:
+      symbol = "";
+      return symbol;
+  }
+};
+let imageUrl;
+const getImageUrl = (description) => {
+  switch (description) {
+    case "Light rain":
+      imageUrl = "https://i.imgur.com/cyjUcoP.png";
+      return imageUrl;
+    case "Light rain shower":
+      imageUrl = "https://i.imgur.com/cyjUcoP.png";
+      return imageUrl;
+    case "Partly cloudy":
+      imageUrl = "https://i.imgur.com/c9GsDBi.png";
+      return imageUrl;
+    case "Moderate snow":
+      imageUrl = "https://i.imgur.com/77GeVE0.png";
+      return imageUrl;
+    case "Clear":
+      imageUrl = "https://i.imgur.com/6nTNzkg.png";
+      return imageUrl;
+    case "Sunny":
+      imageUrl = "https://i.imgur.com/aQAm7Us.png";
+      return imageUrl;
+    default:
+      imageUrl = "";
+      return imageUrl;
+  }
 };
 module.exports = {
   data: new SlashCommandBuilder()
@@ -32,23 +93,31 @@ module.exports = {
     } = await getWeather(query);
     const contentEmbed = new EmbedBuilder();
     contentEmbed.setTitle(
-      `Weather Temperature for ${capitalizeFirstLetter(query)}`
+      `Current Weather Temperature for ${capitalizeFirstLetter(query)}`
     );
     contentEmbed.setDescription(`
-      Temperature: ${temperature}
-      Wind: ${wind}
-      Description: ${description}
+      Temperature: 🌡️ ${temperature}
+      Wind: 💨 ${wind}
+      Weather Condition: ${weatherSymbol(description)} ${description}
     `);
-    forecast.map((f) => {
-      contentEmbed.addFields({ name: "Day", value: f.day, inline: true });
+    forecast.map((f, i) => {
       contentEmbed.addFields({
-        name: "Temperature",
-        value: f.temperature,
+        name: "Date",
+        value: weekday[d.getDay() + i++],
         inline: true,
       });
-      contentEmbed.addFields({ name: "Wind", value: f.wind, inline: true });
+      contentEmbed.addFields({
+        name: "Temperature",
+        value: `🌡️ ${f.temperature}`,
+        inline: true,
+      });
+      contentEmbed.addFields({
+        name: "Wind",
+        value: `💨 ${f.wind}`,
+        inline: true,
+      });
     });
-    contentEmbed.setImage("https://i.imgur.com/ut4L2Qc.png");
+    contentEmbed.setImage(getImageUrl(description));
     contentEmbed.setTimestamp();
     contentEmbed.setFooter({
       text: "Weather BOT",
